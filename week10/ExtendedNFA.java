49bb333f842e7cc0d136723430c80e0fafa40771
Zilvinas Rudzionis <3447630+zrudzionis@users.noreply.github.com>
2024-10-13 19:43:28 +0300

    Add 'Extensions to NFA' answer

--- week10/ExtendedNFA.java
+++ week10/ExtendedNFA.java
@@ -1,46 +1,3 @@
-# Question #1.
-Challenging REs. Construct a regular expression for each of the following languages over the binary alphabet or prove that no such regular expression is possible:
-
-1. All strings except 11 or 111.
-2. Strings with 1 in every odd-number bit position.
-3. Strings with an equal number of 0s and 1s.
-4. Strings with at least two 0s and at most one 1.
-5. Strings that when interpreted as a binary integer are a multiple of 3.
-6. Strings with no two consecutive 1s.
-7. Strings that are palindromes (same forwards and backwards).
-8. Strings with an equal number of substrings of the form 01 and 10.
-
-## Answer
-1. `(0(0|1)*)|(1$)|(10(0|1)*)|(110(0|1)*)`. Here `$` is end of line symbol.
-2. `1(01)*0?`. Here `?` is 0 or 1 symbols.
-3. Not possible since it requires memory. We need to keep track of 0s and 1s count.
-4. `(100+)|(00+1?0*)|(010+)`.
-5. TODO
-6. `(1$)|(0+10*)*|(10+)*`.
-7. Not possible since it requires memory. We need to keep track of 0s and 1s count.
-8. Not possible since it requires memory. We need to keep track of 01s and 10s count.
-
-# Question #2.
-Exponential-size DFA.
-Design a regular expressions of length n such that any DFA that recognizes the same language has an exponential number of states.
-
-## Answer
-Regex: *(0|1){a}*
-
- * *n* - regex length
- * *a* - number
- * *b* - number of bits in *a*
- * D - number of states in DFA
-
-For *a* we have D=2*(2^b) states.
-For added bit to *a* we have D=2*(2^(b + 1)) states and *n* can grow only by one digit so the order of growth is exponential.
-
-
-# Question #3.
-Extensions to NFA. Add to [NFA.java](http://algs4.cs.princeton.edu/54regexp/NFA.java.html) the ability to handle multiway or, wildcard, and the + closure operator.
-
-## Answer
-```
 /******************************************************************************
  *  Compilation:  javac NFA.java
  *  Execution:    java NFA regexp text
@@ -205,4 +162,3 @@ public class ExtendedNFA {
         StdOut.println(nfa.recognizes(txt));
     }
 }
-```
